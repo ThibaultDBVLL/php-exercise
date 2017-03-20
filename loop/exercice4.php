4c224c51425637e07a6cc99c2871424b3f2e227f
thibault <thibault.daboville@gmail.com>
2017-03-20 14:35:24 +0100

    sommaire ajouté partout

--- loop/exercice4.php
+++ loop/exercice4.php
@@ -14,6 +14,16 @@
       $a = $a+$b;
       echo(' ');
       }
+
+      </br>
+    <a href="exercice1.php">Exercice1</a>
+    <a href="exercice2.php">Exercice2</a>
+    <a href="exercice3.php">Exercice3</a>
+    <a href="exercice4.php">Exercice4</a>
+    <a href="exercice5.php">Exercice5</a>
+    <a href="exercice6.php">Exercice6</a>
+    <a href="exercice7.php">Exercice7</a>
+    <a href="exercice8.php">Exercice8</a>
     ?>
   </body>
 </html>
