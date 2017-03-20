81810340730a9d60a9e897191af18e4eaa576229
thibault <thibault.daboville@gmail.com>
2017-03-20 14:31:03 +0100

    exo 8

---
+++ loop/exercice8.php
@@ -0,0 +1,19 @@
+
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    <h1>Exo2</h1>
+      <? php
+      for ($i = 200; $i >=0; $i-=12){
+        echo($i);
+        echo(' ');
+        echo('enfin');
+        echo(' ');
+      }
+      ?>
+  </body>
+</html>
