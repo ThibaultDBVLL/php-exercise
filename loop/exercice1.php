e73724c1d8408551157bc8a555f2ebdab32f5aca
thibault <thibault.daboville@gmail.com>
2017-03-20 11:20:43 +0100

    exercice 1 fait

---
+++ loop/exercice1.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    <h1>Exo1</h1>
+
+    <? php
+      for ($i = 0 ; ; $i++) {
+        if ($i > 10){
+          break;
+        }
+        echo $i;
+      }
+    ?>
+  </body>
+</html>
