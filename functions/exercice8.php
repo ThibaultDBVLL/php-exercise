b12297dba880946dcda18ec57cd907f754981341
thibault <thibault.daboville@gmail.com>
2017-03-21 15:12:30 +0100

    exo8 functions fait

--- functions/exercice8.php
+++ functions/exercice8.php
@@ -8,8 +8,10 @@
   <body>
     <h1>Exo8</h1>
       <?php
-
-
+        function nbr($nb1 = 1, $nb2 = 2, $nb3 = 3){
+          return $nb1 + $nb2 + $nb3;
+        }
+        echo nbr();
       ?>
   </body>
 </html>
