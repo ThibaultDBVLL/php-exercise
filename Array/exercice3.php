9e04f5a004b39648568d0fed202f8dc099a2c5d8
thibault <thibault.daboville@gmail.com>
2017-03-21 15:52:55 +0100

    exo3 array fait

---
+++ Array/exercice3.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    <h1>Exo3</h1>
+      <?php
+      $mois = ["janvier","fevrier","mars","avril","mai","juin","juillet","aout","septembre","octobre","novembre","decembre"];
+      echo $mois[5];
+      ?>
+
+      </br>
+    <a href="exercice1.php">Exercice1</a>
+    <a href="exercice2.php">Exercice2</a>
+    <a href="exercice3.php">Exercice3</a>
+    <a href="exercice4.php">Exercice4</a>
+    <a href="exercice5.php">Exercice5</a>
+    <a href="exercice6.php">Exercice6</a>
+    <a href="exercice7.php">Exercice7</a>
+    <a href="exercice8.php">Exercice8</a>
+  </body>
+</html>
