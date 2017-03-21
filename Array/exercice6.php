0317720d8757fd94ed6d6587c1d946ae7f4fc27c
thibault <thibault.daboville@gmail.com>
2017-03-21 16:24:27 +0100

    exo array 6 fait

---
+++ Array/exercice6.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    <h1>Exo6</h1>
+      <?php
+      $HDF = array("02" =>"Aisne", "59" =>"Nord", "60" =>"Oise", "62" =>"Pas-de-Calais", "80" =>"Somme");
+      echo $HDF[59];
+      ?>
+
+      </br>
+    <a href="exercice1.php">Exercice1</a>
+    <a href="exercice2.php">Exercice2</a>
+    <a href="exercice3.php">Exercice3</a>
+    <a href="exercice4.php">Exercice4</a>
+    <a href="exercice5.php">Exercice5</a>
+    <a href="exercice6.php">Exercice6</a>
+    <a href="exercice7.php">Exercice7</a>
+    <a href="exercice8.php">Exercice8</a>
+  </body>
+</html>
