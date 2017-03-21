64a13c1a04b00b4f0f98471d4d22c2fc106425b8
thibault <thibault.daboville@gmail.com>
2017-03-21 14:07:29 +0100

    exo3 functions fait

---
+++ functions/exercice3.php
@@ -0,0 +1,17 @@
+
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    <h1>Exo3</h1>
+      <?php
+        function concatString($string1, $string2){
+          return $string1.$string2;
+        }
+        echo concatString('coder cest cool! ', 'Surtout en PHP');
+      ?>
+  </body>
+</html>
