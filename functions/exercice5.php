d8a432bd084074f19b80b5a77790ea55f26e1ce8
thibault <thibault.daboville@gmail.com>
2017-03-21 14:28:44 +0100

    exo5 functions fait

---
+++ functions/exercice5.php
@@ -0,0 +1,17 @@
+
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    <h1>Exo5</h1>
+      <?php
+        function concatNbString($nb, $strg){
+          return $nb.$strg;
+        }
+        echo concatNbString(3,'ordis');
+      ?>
+  </body>
+</html>
