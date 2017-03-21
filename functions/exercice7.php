86c250d346881c6d01119f8a5d1573e50bc5c328
thibault <thibault.daboville@gmail.com>
2017-03-21 15:09:12 +0100

    exo7 functions fait

--- functions/exercice7.php
+++ functions/exercice7.php
@@ -6,9 +6,23 @@
     <title></title>
   </head>
   <body>
-    <h1>Exo4</h1>
+    <h1>Exo7</h1>
       <?php
-      
+      function minMaj($age, $genre){
+        if ($age >= 18){
+          $majorite = "majeur(e)";
+        }else{
+          $majorite = "mineur(e)";
+        }
+        if ($genre == "Homme"){
+          $civilit = "Vous etes un Homme";
+        }elseif($genre == "Femme"){
+          $civilit = "Vous etes une Femme";
+        }
+        return $civilit.$majorite;
+      };
+
+      echo minMaj(18, "Femme");
       ?>
   </body>
 </html>
