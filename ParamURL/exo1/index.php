b3d6184172f864d0d83ea8a229e4fb2b8ad9650a
thibault <thibault.daboville@gmail.com>
2017-03-22 12:14:23 +0100

    exo paramURL 1 fait

---
+++ ParamURL/exo1/index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    <h1>Exo1</h1>
+      <?php
+      echo $_GET['prenom']." ";
+      echo $_GET['nom']." ";
+      ?>
+  </body>
+</html>
