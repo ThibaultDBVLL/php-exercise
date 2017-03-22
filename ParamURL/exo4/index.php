1d7669dd3da6fafe6f844fd68925cb502bdf1f33
thibault <thibault.daboville@gmail.com>
2017-03-22 14:18:49 +0100

    exo paramURL 4 fait

---
+++ ParamURL/exo4/index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    <h1>Exo4</h1>
+      <form class="" action="user.php" method="get">
+        <label for="name">Nom :</label>
+        <input type="text" name="name" value=""> </br>
+        <label for="firstName">Prenom :</label>
+        <input type="text" name="firstName" value=""> </br>
+        <input type="submit" name="" value="submit">
+      </form>
+
+      <?php
+
+      ?>
+  </body>
+</html>
