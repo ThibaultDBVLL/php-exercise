d2350ce13aa17588544d573e0f43160d9d0e7086
thibault <thibault.daboville@gmail.com>
2017-03-22 12:23:47 +0100

    exo paramURL 2 fait

---
+++ ParamURL/exo2/index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    <h1>Exo1</h1>
+      <?php
+      echo $_GET['prenom']." ";
+      echo $_GET['nom']." ";
+      if(isset($age)){
+        echo $age;
+      }else{
+        echo "age non renseigné";
+      }
+      ?>
+  </body>
+</html>
