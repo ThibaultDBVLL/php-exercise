f49f4e78415a62de7f6c451ffc59c5b1ca7064d0
thibault <thibault.daboville@gmail.com>
2017-03-22 16:49:36 +0100

    exo form8 fait

---
+++ Form/exo8/index.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    <h1>Exo8</h1>
+    <?php
+      if(!$_POST["civilit"]||!$_POST["name"]||!$_POST["firstName"]){
+      echo'<form class="" action="user.php" method="post">
+        <select class="" name="civilit">
+          <option value="Monsieur">Monsieur</option>
+          <option value="Madame">Madame</option>
+        </select> <br>
+        <label for="name">Nom :</label>
+        <input type="text" name="name" value=""> </br>
+        <label for="firstName">Prenom :</label>
+        <input type="text" name="firstName" value=""> </br>
+        <input type="file" name="file"> </br>
+        <input type="submit" name="" value="submit">
+      </form>';
+    }else{
+        echo $_POST['civilit'];
+        echo $_POST['name'];
+        echo $_POST['firstName'];
+        echo $_POST['file'];
+        if (explode(".", $_POST["file"])[1]==="pdf"){
+          echo"C'est bien un PDF";
+        }else{
+          echo"C'est pas un PDF";
+        }
+      }
+      ?>
+  </body>
+</html>
